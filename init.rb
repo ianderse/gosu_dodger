a9d1b9cd1c84600dc743ff33eb58e2df5b797e54
Ian Andersen <ianderse@mac.com>
2014-05-27 17:07:13 -0600

    change draw_rot to draw

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require 'gosu'
 require './player'
+require './enemy'
 require 'rubygems'
 
 class GameWindow < Gosu::Window
@@ -12,22 +13,21 @@ class GameWindow < Gosu::Window
 		@background = Gosu::Image.new(self, './gfx/blue_stars_800x600.png')
 
 		@player = Player.new(self)
-		@player.warp(450, 550)
+		@enemy = Enemy.new(self)
+		@player.warp(350, 520)
 	end
 
 	def update
         @player.move_left if self.button_down?(Gosu::KbLeft)
         @player.move_right if self.button_down?(Gosu::KbRight)
-        #@player.accelerate if self.button_down?(Gosu::KbUp)
-
-        @player.move
 	end
 
 	def draw
 		@background.draw(0,0,0)
 		@player.draw
+		@enemy.draw
 
-		@font.draw("X: #{@player.x}", 10, 10, 1, 1.0, 1.0, 0xffffff00)
+		@font.draw("Score: #{@player.score}", 10, 10, 1, 1.0, 1.0, 0xffffff00)
 	end
 
 	def button_up(key)
--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
 class Player
 
-	attr_accessor :x, :y
+	attr_accessor :x, :y, :score
 
 	def initialize(window)
 		@image = Gosu::Image.new(window, './gfx/player.png', false)
-		@x = @y = @vel_x = @vel_y = @angle = 0.0
+		@x = @y = 0.0
 		@score = 0
 	end
 
@@ -13,37 +13,22 @@ class Player
 	end
 
 	def move_left
-		if @x > 50
+		if @x > 0
 			@x -= 5
 		else
-			@x = 50
+			@x = 0
 		end
 	end
 
 	def move_right
-		if @x < 750
+		if @x < 700
 			@x += 5
 		else
-			@x = 750
+			@x = 700
 		end
 	end
 
-	def accelerate
-		@vel_x += Gosu::offset_x(@angle, 0.5)
-		@vel_y += Gosu::offset_y(@angle, 0.5)
-	end
-
-	def move
-		@x += @vel_x
-		@y += @vel_y
-		@x %= 800
-		@y %= 600
-
-		@vel_x *= 0.95
-		@vel_y *= 0.95
-	end
-
 	def draw
-		@image.draw_rot(@x, @y, 1, @angle)
+		@image.draw(@x, @y, 1)
 	end
 end
