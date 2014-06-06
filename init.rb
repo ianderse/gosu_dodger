b9c0ff7895e64324f6e6aec27cebf55b5541ac25
Ian Andersen <ianderse@mac.com>
2014-06-06 16:47:40 -0600

    add ability to pause and unpause game

--- init.rb
+++ init.rb
@@ -16,16 +16,26 @@ class GameWindow < Gosu::Window
 		@player.warp(350, 520)
 
 		@enemies = Array.new
+
+		@game_is_paused = false
 	end
 
 	def update
+	if @game_is_paused == false
+
+		#add new enemy to enemy array
 		if rand(100) < 4 and @enemies.size < 10
         	@enemies.push(Enemy.new(self))
         end
 
+        #move enemy
 		@enemies.each { |enemy| enemy.move }
+
+		#move player
         @player.move_left if self.button_down?(Gosu::KbLeft)
         @player.move_right if self.button_down?(Gosu::KbRight)
+
+        #Collision detection
 
         @enemies.each do |enemy|
         	if Gosu::distance(enemy.x, enemy.y, @player.x, @player.y) < 40
@@ -33,6 +43,7 @@ class GameWindow < Gosu::Window
         	end
         end
 
+        #remove enemy from array (and screen) when it hits the bottom of the screen, add to player score
         @enemies.reject! do |enemy|
         	if enemy.bottom?
         		@player.score += 5
@@ -51,8 +62,14 @@ class GameWindow < Gosu::Window
 		@font.draw("Score: #{@player.score}", 10, 10, 1, 1.0, 1.0, 0xffffff00)
 	end
 
+	def game_pause_toggle
+		@game_is_paused = !@game_is_paused
+	end
+
 	def button_up(key)
 		self.close if key == Gosu::KbEscape
+		game_pause_toggle if key == Gosu::KbSpace
+	end
 	end
 end
 
